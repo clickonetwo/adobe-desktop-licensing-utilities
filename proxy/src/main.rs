37b4609259d5d4897e00da2591c3f2a11387f455
Andrew Dorton <adorton@adobe.com>
2020-08-26 13:27:55 -0600

    forward method, headers and body

--- proxy/src/main.rs
+++ proxy/src/main.rs
@@ -1,12 +1,26 @@
 use hyper::{service::{make_service_fn, service_fn}, Body, Client, Request,
-            Response, Server, Uri};
+            Response, Server};
 use std::net::SocketAddr;
 
 async fn serve_req(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
     println!("received request at {:?}", req.uri());
-    let url_str = "http://example.com";
-    let url = url_str.parse::<Uri>().expect("failed to parse url");
-    let res = Client::new().get(url).await?;
+    println!("method {:?}", req.method());
+    println!("headers {:?}", req.headers());
+    // use the echo server for now
+    let url_str = "http://localhost:3000";
+    let mut client_req_builder = Request::builder()
+        .method(req.method())
+        .uri(url_str);
+    for (k, v) in req.headers().iter() {
+        // the hyper client we invoke later will set the host, so we 
+        // don't want to set that here
+        if k == "host" {
+            continue;
+        }
+        client_req_builder = client_req_builder.header(k, v);
+    }
+    let client_req = client_req_builder.body(req.into_body()).expect("error building client request");
+    let res = Client::new().request(client_req).await?;
     Ok(res)
 }
 
@@ -22,7 +36,8 @@ async fn run_server(addr: SocketAddr) {
 }
 
 #[tokio::main]
-async fn main() {
-    let addr = "127.0.0.1:3030".parse::<SocketAddr>().expect("failed to parse server host");
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr = "127.0.0.1:3030".parse::<SocketAddr>()?;
     run_server(addr).await;
+    Ok(())
 }
