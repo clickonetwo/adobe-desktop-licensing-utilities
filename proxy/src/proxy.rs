00d07e01bf457e73fe267ddb4699cfd6b2179295
Andrew Dorton <adorton@adobe.com>
2020-09-01 18:26:03 -0600

    print request body

--- proxy/src/proxy.rs
+++ proxy/src/proxy.rs
@@ -2,30 +2,34 @@ pub mod plain;
 pub mod secure;
 
 use hyper::{Body, Client, Request, Response};
+use futures::TryStreamExt;
 use crate::settings::Settings;
 
 async fn serve_req(req: Request<Body>, conf: Settings) -> Result<Response<Body>, hyper::Error> {
-    println!("received request at {:?}", req.uri());
-    println!("method {:?}", req.method());
-    println!("headers {:?}", req.headers());
+    let (parts, body) = req.into_parts();
+    println!("received request at {:?}", parts.uri);
+    println!("method {:?}", parts.method);
+    println!("headers {:?}", parts.headers);
+    let entire_body = body
+        .try_fold(Vec::new(), |mut data, chunk| async move {
+            data.extend_from_slice(&chunk);
+            Ok(data)
+        })
+        .await?;
+    println!("body {:?}", std::str::from_utf8(&entire_body).unwrap());
     // use the echo server for now
     let lcs_host = conf.proxy.remote_host;
-    let url_str = match req.uri().query() {
-        Some(qstring) => format!("{}{}?{}", lcs_host, req.uri().path(), qstring),
-        None => format!("{}{}", lcs_host, req.uri().path()),
+    let url_str = match parts.uri.query() {
+        Some(qstring) => format!("{}{}?{}", lcs_host, parts.uri.path(), qstring),
+        None => format!("{}{}", lcs_host, parts.uri.path()),
     };
     let mut client_req_builder = Request::builder()
-        .method(req.method())
+        .method(parts.method)
         .uri(url_str);
-    for (k, v) in req.headers().iter() {
-        // the hyper client we invoke later will set the host, so we 
-        // don't want to set that here
-        if k == "host" {
-            continue;
-        }
+    for (k, v) in parts.headers.iter() {
         client_req_builder = client_req_builder.header(k, v);
     }
-    let client_req = client_req_builder.body(req.into_body()).expect("error building client request");
+    let client_req = client_req_builder.body(Body::from(entire_body)).expect("error building client request");
     let res = Client::new().request(client_req).await?;
     Ok(res)
 }
