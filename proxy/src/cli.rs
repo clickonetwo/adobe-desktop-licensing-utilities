d938f354d95c4a842b4ebc2f4bd7d970fe8fcf94
Andrew Dorton <adorton@adobe.com>
2020-09-11 14:46:33 -0600

    cli option help

--- proxy/src/cli.rs
+++ proxy/src/cli.rs
@@ -6,26 +6,33 @@ pub enum Opt {
     /// Start the proxy server
     Start {
         #[structopt(short, long)]
+        /// Path to optional config file
         config_file: Option<String>,
 
         #[structopt(long)]
+        /// Proxy hostname
         host: Option<String>,
 
         #[structopt(long)]
+        /// Remote (licensing server) hostname
         remote_host: Option<String>,
 
         #[structopt(long)]
+        /// Enable SSL
         ssl: bool,
 
         #[structopt(long)]
+        /// Path to SSL certificate
         ssl_cert: Option<String>,
 
         #[structopt(long)]
+        /// Path to SSL private key
         ssl_key: Option<String>,
     },
     /// Create a template config file
     InitConfig {
         #[structopt(short, long, default_value = "config.toml")]
+        /// path to config filename
         out_file: String,
     }
 }
