b318b0d6990f1580b0ed9fdad131c79b60d21f06
Daniel Brotsky <dev@brotsky.com>
2022-06-27 19:36:23 -0700

    Update the licensing and copyrights.
    Start to fix the README files.

--- proxy/src/logging.rs
+++ proxy/src/logging.rs
@@ -1,10 +1,20 @@
 /*
-Copyright 2020 Adobe
-All Rights Reserved.
+Copyright 2022 Daniel Brotsky. All rights reserved.
 
-NOTICE: Adobe permits you to use, modify, and distribute this file in
-accordance with the terms of the Adobe license agreement accompanying
-it.
+All of the copyrighted work in this repository is licensed under the
+GNU Affero General Public License, reproduced in the LICENSE-AGPL file.
+
+Attribution:
+
+Some source files in this repository are derived from files in two Adobe Open
+Source projects: the Adobe License Decoder repository found at this URL:
+    https://github.com/adobe/adobe-license-decoder.rs
+and the FRL Online Proxy repository found at this URL:
+    https://github.com/adobe/frl-online-proxy
+
+The files in those original works are copyright 2022 Adobe and the use of those
+materials in this work is permitted by the MIT license under which they were
+released.  That license is reproduced here in the LICENSE-MIT file.
 */
 use crate::settings::{LogDestination, LogLevel, Settings};
 use eyre::{Result, WrapErr};
