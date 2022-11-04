e86e919217d260ccbe4ea7385cdc0f565cae40a1
Daniel Brotsky <dan@clickonetwo.io>
2022-11-04 15:39:29 -0700

    Build filters for all the new type of Requests.
    Next: make the proxy use the new filters, and convert the cache to use the new requests.

--- adlu-proxy/src/proxy/protocol.rs
+++ adlu-proxy/src/proxy/protocol.rs
@@ -18,10 +18,14 @@ released.  That license is reproduced here in the LICENSE-MIT file.
 */
 
 use eyre::{Result, WrapErr};
+use log::error;
 use warp::{Filter, Rejection};
+
+use adlu_base::Timestamp;
 
 use super::config::Config;
 
+#[derive(Clone, Debug)]
 pub enum RequestType {
     FrlActivation,
     FrlDeactivation,
@@ -30,7 +34,9 @@ pub enum RequestType {
     Unknown,
 }
 
+#[derive(Clone, Debug)]
 pub struct Request {
+    pub timestamp: Timestamp,
     pub kind: RequestType,
     pub source_ip: Option<std::net::SocketAddr>,
     pub method: http::method::Method,
@@ -38,53 +44,102 @@ pub struct Request {
     pub query: Option<String>,
     pub body: Option<String>,
     pub content_type: String,
-    pub accept_type: String,
-    pub api_key: String,
+    pub accept_type: Option<String>,
+    pub accept_language: Option<String>,
+    pub api_key: Option<String>,
     pub request_id: Option<String>,
     pub session_id: Option<String>,
     pub authorization: Option<String>,
 }
 
 impl Request {
-    pub fn generic_filter() -> impl Filter<Extract = (Self,), Error = Rejection> + Clone {
-        warp::any()
-            .and(warp::filters::addr::remote())
+    pub fn frl_activation_filter(
+    ) -> impl Filter<Extract = (Self,), Error = Rejection> + Clone {
+        warp::post()
+            .and(warp::path!("asnp" / "frl_connected" / "values" / "v2"))
+            .and(required_header("X-Api-Key"))
+            .and(required_header("X-Request-Id"))
+            .and(Self::request_filter(RequestType::FrlActivation))
+    }
+
+    pub fn frl_deactivation_filter(
+    ) -> impl Filter<Extract = (Self,), Error = Rejection> + Clone {
+        warp::delete()
+            .and(warp::path!("asnp" / "frl_connected" / "v1"))
+            .and(required_header("X-Api-Key"))
+            .and(required_header("X-Request-Id"))
+            .and(required_query())
+            .and(Self::request_filter(RequestType::FrlDeactivation))
+    }
+
+    pub fn nul_license_filter(
+    ) -> impl Filter<Extract = (Self,), Error = Rejection> + Clone {
+        warp::post()
+            .and(warp::path!("asnp" / "nud"))
+            .and(required_header("X-Api-Key"))
+            .and(required_header("X-Request-Id"))
+            .and(required_header("X-Session-Id"))
+            .and(required_header("Authorization"))
+            .and(Self::request_filter(RequestType::FrlDeactivation))
+    }
+
+    pub fn log_upload_filter() -> impl Filter<Extract = (Self,), Error = Rejection> + Clone
+    {
+        warp::post()
+            .and(warp::path!("ulecs" / "v1"))
+            .and(required_header("X-Api-Key"))
+            .and(required_header("Authorization"))
+            .and(Self::request_filter(RequestType::FrlDeactivation))
+    }
+
+    pub fn unknown_filter() -> impl Filter<Extract = (Self,), Error = Rejection> + Clone {
+        warp::any().and(Self::request_filter(RequestType::Unknown))
+    }
+
+    fn request_filter(
+        kind: RequestType,
+    ) -> impl Filter<Extract = (Self,), Error = Rejection> + Clone {
+        warp::filters::addr::remote()
             .and(warp::method())
             .and(warp::path::full())
-            .and(optional_raw_query_filter())
+            .and(optional_raw_query())
             .and(warp::header::<String>("Content-Type"))
-            .and(warp::header::<String>("Accept"))
-            .and(warp::header::<String>("X-Api-Key"))
+            .and(warp::filters::header::optional::<String>("Accept"))
+            .and(warp::filters::header::optional::<String>("Accept-Language"))
+            .and(warp::filters::header::optional::<String>("X-Api-Key"))
             .and(warp::filters::header::optional::<String>("X-Request-Id"))
             .and(warp::filters::header::optional::<String>("X-Session-Id"))
             .and(warp::filters::header::optional::<String>("Authorization"))
             .and(warp::body::content_length_limit(32_000))
             .and(warp::body::bytes())
             .map(
-                |source_ip,
-                 method,
-                 path: warp::path::FullPath,
-                 query,
-                 content_type,
-                 accept_type,
-                 api_key,
-                 request_id,
-                 session_id,
-                 authorization,
-                 body: bytes::Bytes| {
+                move |source_ip,
+                      method,
+                      path: warp::path::FullPath,
+                      query,
+                      content_type,
+                      accept_type,
+                      accept_language,
+                      api_key,
+                      request_id,
+                      session_id,
+                      authorization,
+                      body: bytes::Bytes| {
                     let body: Option<String> = if body.is_empty() {
                         None
                     } else {
                         Some(String::from_utf8_lossy(&body).to_string())
                     };
                     Self {
-                        kind: RequestType::Unknown,
+                        timestamp: Timestamp::now(),
+                        kind: kind.clone(),
                         source_ip,
                         method,
                         path: path.as_str().to_string(),
                         query,
                         content_type,
                         accept_type,
+                        accept_language,
                         api_key,
                         request_id,
                         session_id,
@@ -108,10 +163,16 @@ impl Request {
         builder = builder
             .header("User-Agent", super::agent())
             .header("Accept-Encoding", "gzip, deflate, br")
-            .header("Accept", &self.accept_type)
-            .header("Accept-Language", "en-us")
-            .header("Content-Type", &self.content_type)
-            .header("X-Api-Key", &self.api_key);
+            .header("Content-Type", &self.content_type);
+        if let Some(accept_type) = &self.accept_type {
+            builder = builder.header("Accept", accept_type)
+        }
+        if let Some(accept_language) = &self.accept_language {
+            builder = builder.header("Accept-Language", accept_language);
+        }
+        if let Some(api_key) = &self.api_key {
+            builder = builder.header("X-Api-Key", api_key);
+        }
         if let Some(request_id) = &self.request_id {
             builder = builder.header("X-Request-Id", request_id);
         }
@@ -135,11 +196,67 @@ impl Request {
     }
 }
 
-fn optional_raw_query_filter(
+fn optional_raw_query(
 ) -> impl Filter<Extract = (Option<String>,), Error = Rejection> + Clone {
     warp::query::raw()
         .map(Some)
         .or_else(|_| async { Ok::<(Option<String>,), Rejection>((None,)) })
+}
+
+fn required_query() -> impl Filter<Extract = (), Error = Rejection> + Clone {
+    warp::query::raw().map(|_| {}).untuple_one()
+}
+
+fn required_header(
+    key: &'static str,
+) -> impl Filter<Extract = (), Error = Rejection> + Clone {
+    warp::header::<String>(key).map(|_| {}).untuple_one()
+}
+
+pub struct Response {
+    pub timestamp: Timestamp,
+    pub kind: RequestType,
+    pub status: http::status::StatusCode,
+    pub body: Option<String>,
+    pub content_type: Option<String>,
+    pub request_id: Option<String>,
+    pub session_id: Option<String>,
+}
+
+impl Response {
+    pub async fn from_network(req: &Request, resp: reqwest::Response) -> Result<Self> {
+        let timestamp = Timestamp::now();
+        let kind = req.kind.clone();
+        let status = resp.status();
+        let content_type = if let Some(val) = resp.headers().get("Content-Type") {
+            Some(val.to_str().wrap_err("Content-Type is not valid")?.to_string())
+        } else {
+            None
+        };
+        let request_id = if let Some(val) = resp.headers().get("X-Request-Id") {
+            Some(val.to_str().wrap_err("X-Request-Id is not valid")?.to_string())
+        } else {
+            None
+        };
+        let session_id = if let Some(val) = resp.headers().get("X-Session-Id") {
+            Some(val.to_str().wrap_err("X-Request-Id is not valid")?.to_string())
+        } else {
+            None
+        };
+        let content = resp.text().await.wrap_err("Failure to receive body")?;
+        let body = if content_type.is_some() {
+            Some(content)
+        } else {
+            if !content.is_empty() {
+                error!(
+                    "Response has body but no content type, ignoring it: {}",
+                    &content
+                );
+            }
+            None
+        };
+        Ok(Self { timestamp, kind, status, body, content_type, request_id, session_id })
+    }
 }
 
 #[cfg(test)]
@@ -147,8 +264,8 @@ mod test {
     use std::net;
 
     #[tokio::test]
-    async fn generic_post_json_body() {
-        let filter = super::Request::generic_filter();
+    async fn protocol_generic_post_json_body() {
+        let filter = super::Request::unknown_filter();
         let req = warp::test::request()
             .remote_addr("127.0.0.1:18040".parse::<net::SocketAddr>().unwrap())
             .method("POST")
@@ -163,9 +280,25 @@ mod test {
             .body(r#"{"key1": "value1", "key2": 300}"#)
             .filter(&filter)
             .await
-            .expect("generic_request filter failed a JSON post");
+            .expect("unknown_filter failed a JSON post");
         assert_eq!(req.query, None);
         assert_eq!(req.authorization, None);
-        assert_eq!(req.api_key, "ngl_photoshop1");
+        assert_eq!(req.api_key.expect("No API Key"), "ngl_photoshop1");
+    }
+
+    #[tokio::test]
+    async fn protocol_missing_content_type_reject() {
+        let filter = super::Request::unknown_filter();
+        let req = warp::test::request()
+            .remote_addr("127.0.0.1:18040".parse::<net::SocketAddr>().unwrap())
+            .method("POST")
+            .path("/asnp/v1")
+            .header("User-Agent", "TestAgent")
+            .header("Accept", "application/json")
+            .header("Accept-Encoding", "deflate")
+            .body(r#"{"key1": "value1", "key2": 300}"#)
+            .filter(&filter)
+            .await;
+        assert!(req.is_err());
     }
 }
